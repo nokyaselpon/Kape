b688247f43dda1d6f06d11b8ce501fbc5f7aab99
nokyaselpon <75005487+nokyaselpon@users.noreply.github.com>
2022-11-21 16:13:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const hamburger = document.getElementById("hamburger")
 const navmenu = document.getElementById("navmenu")
+const btnsearch = document.getElementById("btn-search")
+const searchbar = document.getElementById("search-bar")
+const inputsearch = document.getElementById("input-search")
 
-let clicks = () => {
+hamburger.addEventListener("click", (gg) =>{
+
+    if(gg.target.id === 'hamburger'){
     navmenu.classList.toggle("active")
-}
-
-hamburger.addEventListener("click",clicks)
-
-document.onclick = function(clicks2){
-    if(clicks2.target.id !== 'hamburger' && clicks2.target.id !== 'navmenu'){
-        navmenu.classList.remove("active")
     }
-}
+})
+
+btnsearch.addEventListener("click", () => {
+    searchbar.classList.toggle("active")
+})
+
+document.addEventListener('click', (gg2) => {
+    if(gg2.target.id !== 'hamburger' && gg2.target.id !== 'navmenu')
+    {
+        navmenu.classList.remove("active")
+        hamburger.classList.toggle("active")
+    }
+
+    if(gg2.target.id !== 'btn-search' && gg2.target.id !== 'input-search')
+    {
+        searchbar.classList.remove("active")
+    }
+})
+
+
+
+
+
